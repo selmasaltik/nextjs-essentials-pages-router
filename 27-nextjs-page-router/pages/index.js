660ba2b048bbe93a0c2278d6b8475f4962da9fd0
selmasaltik <selmasaltiik@gmail.com>
2024-11-19 20:43:43 +0300

    Added Link to Home Page

--- 27-nextjs-page-router/pages/index.js
+++ 27-nextjs-page-router/pages/index.js
@@ -1,7 +1,15 @@
 // our-domain.com/
 
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function HomePage() {
-    return <h1>The Home Page</h1>
+    return (
+        <Fragment>
+            <h1>The Home Page</h1>
+            <Link href='/news'>Browse The News</Link>
+        </Fragment>
+    ); 
 }
 
 export default HomePage;
--- 27-nextjs-page-router/pages/news/index.js
+++ 27-nextjs-page-router/pages/news/index.js
@@ -9,7 +9,7 @@ function NewsPage() {
             <h1>The News Page</h1>
             <ul>
                 <li>
-                    <Link href='nextjs-is-a-great-framework'>
+                    <Link href='/news/nextjs-is-a-great-framework'>
                         NextJS Is A Great Framework
                     </Link>
                 </li>
